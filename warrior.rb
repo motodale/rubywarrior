762b4594e073acda2a61e29e10d0051e6acae938
Dale Tibbetts <dale.tibbetts@banno.com>
2013-09-30 15:36:01 -0500

    level 6

--- warrior.rb
+++ warrior.rb
@@ -23,3 +23,34 @@ class Player
   end
 
 end
+
+
+
+class Player
+  def play_turn(warrior)
+  @health = nil
+  space = warrior.feel
+  ecaps = warrior.feel(:backward)
+
+  @health = warrior.health unless @health
+  ouch = @health > warrior.health
+
+
+     if space.empty?
+        if warrior.health < @health && !ouch
+          warrior.rest!
+        else
+          warrior.walk!
+        end
+     else
+      if space.captive?
+        warrior.rescue!
+      else
+        warrior.attack!
+      end
+    end
+     @health = warrior.health
+  end
+
+
+end
