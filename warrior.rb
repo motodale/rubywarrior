35c9e0de45906d9b9ed5a07f3ff0a9cac8d8f22d
Dale Tibbetts <dale.tibbetts@banno.com>
2013-10-02 13:15:06 -0500

    reworked

--- warrior.rb
+++ warrior.rb
@@ -24,36 +24,50 @@ class Player
 
 end
 
-
-
-
 class Player
+
+Bleeding = 20 * 0.4
+Bezerk = 20 * 0.7
+
   def play_turn(warrior)
 
   space = warrior.feel
   ecaps = warrior.feel(:backward)
-
+  @wall = ecaps.wall?
   @health = warrior.health unless @health
   ouch = @health > warrior.health
 
-     if space.empty?
-        if warrior.health <= Bleeding
-          warrior.walk! :backward
-        else
+    if ecaps.empty?
+      if ecaps.captive?
+        warrior.rescue! :backward
+      else
+         if @wall == true
           warrior.walk!
+         else
+          warrior.walk! :backward
+         end
+      end
+    else @wall == true
+      if space.empty?
+          if warrior.health <= Bleeding
+            warrior.walk! :backward
+          else
+            warrior.walk!
+          end
+        else
+          if warrior.health < Bezerk
+            warrior.rest!
+          else
+            warrior.walk!
+          end
+        if space.captive?
+          warrior.rescue!
+        else
+          warrior.attack!
         end
-     else
-      if warrior.health < Bezerk
-        warrior.rest!
-      else
-        warrior.awalk!
-      end
-      if space.captive?
-        warrior.rescue!
-      else
-        warrior.attack!
-      end
+        end
     end
      @health = warrior.health
+
   end
 end
